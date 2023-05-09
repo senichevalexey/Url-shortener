11169afff4fa00235f6d9a2e49d5509dc20b65c5
Alexey <senichevalexey@gmail.com>
2023-05-09 23:41:20 +0800

    Added POST functionality

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { ShortURLType } from './types';
+import { ShortURLType, UrlPairType } from './types';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { URLs } from './schemas/url.schema';
 import { UrlDto } from './dto/url.dto';
+import { generate } from 'shortid';
 
 @Injectable()
 export class AppService {
@@ -15,10 +16,32 @@ export class AppService {
 
   async shorten(urlDto: UrlDto): Promise<ShortURLType> {
     const longUrl = urlDto.url;
-    const shortUrl = '123';
-    const urlPair = new this.urlModel({ longUrl, shortUrl });
-    await urlPair.save();
+    const duplicate = await this.getDuplicate({ longUrl });
 
-    return { id: shortUrl };
+    if (duplicate) {
+      return { id: duplicate.shortUrl };
+    }
+
+    const id = await this.getShortId(longUrl);
+    return { id };
+  }
+
+  private getDuplicate(url: Partial<UrlPairType>): Promise<UrlPairType> {
+    return this.urlModel.findOne(url);
+  }
+
+  private async getShortId(longUrl: string): Promise<string> {
+    while (true) {
+      const id = generate();
+
+      // check possible collisions
+      const duplicate = await this.getDuplicate({ shortUrl: id });
+      if (!duplicate) {
+        const urlPair = new this.urlModel({ longUrl, shortUrl: id });
+        await urlPair.save();
+
+        return id;
+      }
+    }
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,8 @@
+export type UrlPairType = {
+  longUrl: string;
+  shortUrl: string;
+};
+
 export type ShortURLType = {
   id: string;
 };
